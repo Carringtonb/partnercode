6a61fe882dfd4d12d7565eed5fc24d74f02d0bfe
carrington <carringtonbeard@live.com>
2020-01-07 15:48:43 -0800

    fixed syntax errors in JavaScript code

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 'use strict'; 
 
-var sports = prompt('Do you enjoy playing sports?');
-    if(sports.toLowerCase() === 'yes'){
-        alert('That is wonderful!');
-    }else{
-        alert('you are wrong');
-    }
+var childhood = prompt('Did I grow up in Seattle?');
+if(childhood.toLowerCase() === 'yes'){
+  alert('No I did not');
+}else{
+  alert('you are correct');
+}
 
-var city = prompt('Is Seattle the capitol of Washington?');
-    if(city.toUpperCase() === 'YES'){
-        alert('You are incorrect');
-    }else{
-        alert('You are correct');
-    }
-var name = "cat"; 
+var job = prompt('Have I worked in tech before?');
+if(job.toUpperCase() === 'YES'){
+  alert('You are incorrect');
+}else{
+  alert('You are correct');
+}
+
+var age = prompt('Am I 26 years of age?');
+if(age.toLowerCase() === 'yes'){
+  alert('You are correct');
+}else{
+  alert('You are incorrect');
+}
+
+var school = prompt('Did I attend College?');
+if(school.toLowerCase() === 'yes'){
+  alert('You are correct');
+}else{
+  alert('You are incorrect');
+}
+
+var employer = prompt('Is my ideal employer Lockheed?');
+if(employer.toUpperCase() === 'YES'){
+  alert('You are correct');
+}else{
+  alert('You are incorrect');
+}

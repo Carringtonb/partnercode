56c0910a320c071c7ee6bee3103307312bb184d2
Carrington Beard <carringtonbeard@Carringtons-MBP.localdomain>
2020-01-07 14:49:23 -0800

    fixing location of files

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'; 
+
+var sports = prompt('Do you enjoy playing sports?');
+    if(sports.toLowerCase() === 'yes'){
+        alert('That is wonderful!');
+    }else{
+        alert('you are wrong');
+    }
+
+var city = prompt('Is Seattle the capitol of Washington?');
+    if(city.toUpperCase() === 'YES'){
+        alert('You are incorrect');
+    }else{
+        alert('You are correct')
+    }
+        
